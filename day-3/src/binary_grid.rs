c57cb479a99b9a2286bebb3c1775037397ac5c81
Sandile Keswa <me@sandile.io>
2021-12-04 18:51:59 -0500

    Finished documenting binary grid.

--- day-3/src/binary_grid.rs
+++ day-3/src/binary_grid.rs
@@ -35,57 +35,71 @@ impl BinaryGrid {
 
         let width = bits.iter().map(|row| row.len()).max().unwrap_or(0);
 
-        Ok(BinaryGrid {
-            bits: bits,
-            width: width,
-        })
+        Ok(BinaryGrid { bits, width })
     }
 
+    /// Returns a [BitSequence] of the least common bit in each column.
     pub fn least_common_bit_in_each_column(&self) -> BitSequence {
         (0..self.width)
             .map(|column_index| self.least_common_bit_in_column(column_index).unwrap())
             .collect::<BitSequence>()
     }
 
+    /// Returns a [BitSequence] of the most common bit in each column.
     pub fn most_common_bit_in_each_column(&self) -> BitSequence {
         (0..self.width)
             .map(|column_index| self.most_common_bit_in_column(column_index).unwrap())
             .collect::<BitSequence>()
     }
 
+    /// Summarizes an entire column of [Bit] in a [BitAggregator], returning
+    /// the [BitAggregator] thereafter.
     fn aggregate_bits_in_column(&self, column_index: usize) -> BitAggregator {
         if column_index >= self.width {
-            return BitAggregator::empty();
+            return BitAggregator::zero();
         }
 
         self.bits
             .iter()
             .map(|bits| bits[column_index])
             .map(BitAggregator::from)
-            .reduce(BitAggregator::aggregate)
-            .unwrap_or_else(BitAggregator::empty)
+            .reduce(BitAggregator::combine)
+            .unwrap_or_else(BitAggregator::zero)
     }
 
+    /// Returns the most common [Bit] in the column indicated by `column_index`,
+    /// returning [Option::None] if no such [Bit] exists.
     fn most_common_bit_in_column(&self, column_index: usize) -> Option<Bit> {
         self.aggregate_bits_in_column(column_index).most_common()
     }
 
+    /// Returns the least common [Bit] in the column indicated by
+    /// `column_index`, returning [Option::None] if no such [Bit] exists.
     fn least_common_bit_in_column(&self, column_index: usize) -> Option<Bit> {
         self.aggregate_bits_in_column(column_index).least_common()
     }
 }
 
+/// Module encupsulating bitwise logic used by the [super::BinaryGrid].
 mod bit {
+    /// Character representing bitwise one.
     const ONE: char = '1';
+
+    /// Character representing bitwise zero.
     const ZERO: char = '0';
 
+    /// Enumerates both possible vlaues for a bit.
     #[derive(Clone, Copy, Debug, PartialEq)]
     pub enum Bit {
+        /// Enum representation of a bitwise one.
         One,
+        /// Enum representation of a bitwise zero.
         Zero,
     }
 
     impl Bit {
+        /// Attempts to interpret the given [char] as a [Bit], returning
+        /// [Option::None] if interpretation fails.
         pub fn from(bit_char: char) -> Option<Bit> {
             match bit_char {
                 ONE => Some(Bit::One),
@@ -95,25 +109,31 @@ mod bit {
         }
     }
 
+    /// Utility type used to summarize [Bit] collections.
+    ///
+    /// The `0` field refers to the number of [Bit::Zero] instances in a
+    /// collection.
+    ///
+    /// The `1` field refers to the number of [Bit::One] instances in a
+    /// collection.
     #[derive(Debug, PartialEq)]
     pub struct BitAggregator(usize, usize);
 
     impl BitAggregator {
-        pub fn aggregate(a: BitAggregator, b: BitAggregator) -> BitAggregator {
+        /// Combines two instances of [BitAggregator] by summing their
+        /// respective fields within a new [BitAggregator] instance.
+        pub fn combine(a: BitAggregator, b: BitAggregator) -> BitAggregator {
             BitAggregator(a.0 + b.0, a.1 + b.1)
         }
 
-        pub fn empty() -> BitAggregator {
+        /// Creates a new [BitAggregator] both its fields set to `0`.
+        pub fn zero() -> BitAggregator {
             BitAggregator(0, 0)
         }
 
-        pub fn from(bit: Bit) -> BitAggregator {
-            match bit {
-                Bit::One => BitAggregator(0, 1),
-                Bit::Zero => BitAggregator(1, 0),
-            }
-        }
-
+        /// Returns the [Bit] indicated to be the least common according to this
+        /// [BitAggregator], returning [Option::None] if no such [Bit] is
+        /// indicated.
         pub fn least_common(&self) -> Option<Bit> {
             if self.0 < self.1 {
                 Some(Bit::Zero)
@@ -124,6 +144,9 @@ mod bit {
             }
         }
 
+        /// Returns the [Bit] indicated to be the most common according to this
+        /// [BitAggregator], returning [Option::None] if no such [Bit] is
+        /// indicated.
         pub fn most_common(&self) -> Option<Bit> {
             if self.0 > self.1 {
                 Some(Bit::Zero)
@@ -135,6 +158,16 @@ mod bit {
         }
     }
 
+    impl From<Bit> for BitAggregator {
+        fn from(bit: Bit) -> Self {
+            match bit {
+                Bit::One => BitAggregator(0, 1),
+                Bit::Zero => BitAggregator(1, 0),
+            }
+        }
+    }
+
+    /// Represents an ordered collection of [Bit] instances.
     #[derive(Debug, PartialEq)]
     pub struct BitSequence(Vec<Bit>);
 
